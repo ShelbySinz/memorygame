34b7d730b83963ad346a3635ff73fb9f51134caa
Shelby2 <shelbsjb@hotmail.com>
2019-04-07 20:45:22 -0600

    fixed background image

--- memorygame/src/components/Jumbotron.js
+++ memorygame/src/components/Jumbotron.js
@@ -5,18 +5,24 @@ const styles = {
 jumbotron: {
    textAlign: "center",
    backgroundSize: "100%",
-   backgroudPosition: "center",
+   backgroundPosition: "center",
    backgroundRepeat: "noRepeat",
+   color: "white",
    backgroundImage: `url( ${background}) `
 }
 }
 function Jumbotron() {
   return (
     <div className="jumbotron" style = {styles.jumbotron}>
+     <br></br>
       <h1 >Memory Game</h1>
      <strong> <p>
         Click on an image earn a point but becareful if you click on the same image you lose!
       </p></strong>
+      <br>
+     </br>
+     <br>
+     </br>
     </div>
   );
 }
